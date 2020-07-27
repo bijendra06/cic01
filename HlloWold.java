f595cdd00f7eaa869495e442e805ecdc1c160d9a
“bijendra06” <bijendrak428@gmail.com>
2020-07-27 11:31:10 +0530

    Master branch updation

--- HlloWold.java
+++ HlloWold.java
@@ -3,7 +3,7 @@ public class HelloWorld
 
      public static void main(String[] args)
             {
-                System.out.println("Hello to Terminal Command");
+                System.out.println("this is my master branch");
              }
 
 }

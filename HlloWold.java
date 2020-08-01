5f1e3e69fa1333b92216dac2bc24aa1ef0d22d21
“bijendra06” <bijendrak428@gmail.com>
2020-08-01 18:02:42 +0530

    replace line

--- HlloWold.java
+++ HlloWold.java
@@ -4,7 +4,7 @@ public class HelloWorld
      public static void main(String[] args)
             {
                 System.out.println("this is my master branch");
-                System.out.pritln("bijendra");
+                System.out.pritln("this is my develop branch");
 
              }
 

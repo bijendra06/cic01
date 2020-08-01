dfe388f1dfdaaac82ad2b5805697440af23c46f8
“bijendra06” <bijendrak428@gmail.com>
2020-08-01 15:02:27 +0530

    first commit aad line

--- HlloWold.java
+++ HlloWold.java
@@ -4,8 +4,7 @@ public class HelloWorld
      public static void main(String[] args)
             {
                 System.out.println("this is my master branch");
-                System.out.println("this is my master branch");
-
+                System.out.println("that is my branch">
              }
 
 }
